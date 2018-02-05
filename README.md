37e9b78473f52b27372a943f7a3c6ea8788f1e35
Angelo Manganiello <angelo.mang@libero.it>
2018-02-05 16:49:49 +0100

    toolchain with electron

---
+++ README.md
@@ -0,0 +1,3 @@
+# electron-toolchain
+
+A toolchain to generate an electron exe for win32 in dev or prod mode 
---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var gulp = require('gulp');
+
+require('require-dir')('./task');
+
+
+gulp.task('default', ['electron']);
